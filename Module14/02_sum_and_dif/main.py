776dfbefc8e492f8fe90da1e6df7e23f82122bb7
Fantom <andtorg55@yandex.ru>
2023-10-10 19:12:24 +0500

    completed work

--- Module14/02_sum_and_dif/main.py
+++ Module14/02_sum_and_dif/main.py
@@ -1,1 +1,25 @@
 # TODO здесь писать код
+number = int(input('Введите число: '))
+suma = 0
+def suma_number(number):
+   global suma
+   while number > 0:
+       digit = number % 10
+       suma = suma + digit
+       number = number // 10
+   print('Сумма чисел:', suma)
+
+suma_number(number)
+digitcount = 0
+def count_number(number):
+   global digitcount
+   countnumber = number
+   while countnumber > 0:
+       digitcount += 1
+       countnumber = countnumber // 10
+   print('Кол-во цифр в числе:', digitcount)
+count_number(number)
+def differ(suma, digitcount):
+   diff = suma - digitcount
+   print('Разность суммы и кол-ва цифр:', diff)
+differ(suma, digitcount)
--- Module14/04_coin2/main.py
+++ Module14/04_coin2/main.py
@@ -1,1 +1,13 @@
 # TODO здесь писать код
+def find_monetka(c1,c2,r):
+    if c1<=r and c2<=r:
+        print('где-то рядом')
+    else:
+        print('в области нет')
+print('Введите координаты монетки:')
+x=float(input("X: "))
+y=float(input("Y: "))
+r=int(input("Введите радиус: "))
+
+find_monetka(x,y,r)
+
