59243fe0b8a0bc0bdf45c4406b5de5f00488d37d
Fantom <andtorg55@yandex.ru>
2023-11-07 18:16:01 +0500

    completed work

--- Module15/02_tournament/main.py
+++ Module15/02_tournament/main.py
@@ -3,16 +3,15 @@ def display_result(participants_names):
   print('Первый день:', participants_names)
 
 
-def get_participants_names(names):
-  names_list = ['Артемий', 'Борис', 'Влад', 'Гоша', 'Дима', 'Евгений', 'Женя', 'Захар']
+def get_participants_names(names_list):
   names = []
   for i in range(0, len(names_list) - 1, 2):
     names.append(names_list[i])
 
-    return names
+  return names
 
 
 if __name__ == '__main__':
-  names_list = []
+  names_list = ['Артемий', 'Борис', 'Влад', 'Гоша', 'Дима', 'Евгений', 'Женя', 'Захар']
   participants_names = get_participants_names(names_list)
   display_result(participants_names)
