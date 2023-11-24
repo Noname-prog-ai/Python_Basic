864a75b24d44c71d97698b25ba7d6ecefb77d6a2
Fantom <andtorg55@yandex.ru>
2023-11-24 17:44:17 +0500

    completed work

--- Module16/06_roller_skates/main.py
+++ Module16/06_roller_skates/main.py
@@ -1,1 +1,20 @@
 # TODO здесь писать код
+n = []
+k = []
+count_skates = int(input('Введите кол-во роликов: '))
+counter = 0
+
+for i in range(1, count_skates + 1):
+    print('Размер', i, 'пары: ', end = '')
+    n.append(int(input('')))
+count_people = int(input('\nКоличесво людей: '))
+for i in range(1, count_people + 1):
+    print('Размер ноги', i, 'человека: ', end = '')
+    k.append(int(input('')))
+for num in k:
+    for j in range(len(n)):
+        if n[j] >= num:
+            n.remove(n[j])
+            counter += 1
+            break
+print('\nНаибольшее количество людей, которые могут взять ролики:', counter)
--- Module16/08_simmetrical_seq/main.py
+++ Module16/08_simmetrical_seq/main.py
@@ -1,1 +1,18 @@
 # TODO здесь писать код
+n = int(input("Кол-во чисел: "))
+arr = []
+for i in range(n):
+    arr.append(int(input("Число: ")))
+
+if arr == arr[::-1]:
+    print("Нужно приписать чисел: 0")
+else:
+    copy_arr = arr.copy()
+    count = 0
+    while copy_arr != copy_arr[::-1]:
+        count += 1
+        copy_arr = arr.copy()
+        for i in range(count):
+            copy_arr.append(copy_arr[count-i-1])
+            print(f"Нужно приписать чисел: {count}")
+            print(f"Сами числа: {copy_arr[-count:][::-1]}")
