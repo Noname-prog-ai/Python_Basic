14614cbd228d1190440f19205f72e19e28f3f98c
Fantom <andtorg55@yandex.ru>
2023-12-20 19:53:33 +0500

    finished work

--- Module17/03_random_competition/main.py
+++ Module17/03_random_competition/main.py
@@ -1,1 +1,15 @@
 # TODO здесь писать код
+import random
+
+
+def main():
+  team1 = [round(random.uniform(5, 10), 2) for i in range(20)]
+  team2 = [round(random.uniform(5, 10), 2) for i in range(20)]
+  winners = list(map(max, team1, team2))
+
+  print(f"Первая команда: {team1}")
+  print(f"Вторая команда: {team2}")
+  print(f"Победители тура: {winners}")
+
+if __name__ == '__main__':
+  main()
--- Module17/06_two_dimensional_list/main.py
+++ Module17/06_two_dimensional_list/main.py
@@ -1,1 +1,6 @@
 # TODO здесь писать код
+cols = 3; rows = 4
+
+result = [ list(range(x, x + rows * (cols - 1) + 1, rows)) for x in range(1, rows + 1) ]
+
+print(result)
