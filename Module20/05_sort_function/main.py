3ba75427927a3e14183c23412f733e6bc5d715ec
Fantom <andtorg55@yandex.ru>
2024-02-01 18:37:31 +0500

    finished work

--- Module20/05_sort_function/main.py
+++ Module20/05_sort_function/main.py
@@ -1,22 +1,8 @@
 # TODO здесь писать код
 def tpl_sort(tpl):
-  for a in tpl:
-    if not a is int:
+  for element in tpl:
+    if not isinstance(element, int):
       return tpl
-  lst=list(tpl)
-  c = 0
-  k = 1
-  sz = len(lst)
-  while True:
-    c = 0
-    for i in range(sz - k):
-      if lst[i] > lst[i + 1]:
-        lst[i], lst[i + 1] = lst[i + 1], lst[i]
-        c += 1
-    if c == 0:
-      return tuple(lst)
-    k += 1
+  return tuple(sorted(tpl))
 
-tpl = (6, 3, -1, 8, 4, 10, -5)
-tpls = sorted(tpl)
-print(tpl_sort(tpls))
+print(tpl_sort((6, 3, -1, 8, 4, 10, -5)))
