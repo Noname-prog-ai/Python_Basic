14649df498dd2fa77ed3b75a82cb804359c864bc
Fantom <andtorg55@yandex.ru>
2024-02-12 18:48:53 +0500

    finished work

--- Module23/01_names_2/main.py
+++ Module23/01_names_2/main.py
@@ -1,1 +1,17 @@
 # TODO здесь писать код
+import datetime
+
+with open('people.txt', 'r', encoding='utf-8') as people_file:
+    name_line = people_file.read().split('\n')
+    print(name_line)
+    res = ''.join(name_line)
+    try:
+        for number, name in enumerate(name_line):
+            if len(name) < 3:
+                raise ValueError(f'менее трёх символов в строке {number + 1}')
+    except ValueError:
+        print(f'менее трёх символов в строке {number + 1}')
+        with open('errors.txt', 'a', encoding='utf-8') as rec:
+            print(f'{datetime.datetime.now()} менее трёх символов в строке {number + 1}', file=rec)
+    finally:
+        print(f'Общее количество символов: {len(res)}')
--- Module23/05_reliable_calculations/main.py
+++ Module23/05_reliable_calculations/main.py
@@ -1,7 +1,27 @@
 # TODO здесь писать код
 
 # Здесь создайте функцию get_sage_sqrt
+from math import sqrt
 
+
+def get_sage_sqrt(num):
+    try:
+        if isinstance(num, (int, float)):
+            if num < 0:
+                raise ValueError('число должно быть положительным')
+            elif num == 0:
+                raise ZeroDivisionError('у нуля нет корня.')
+            else:
+                res = sqrt(num)
+                str_num = str(res)
+                if '.' in str_num[-2]:
+                    return int(res)
+                else:
+                    return res
+        else:
+            raise TypeError('ожидали число получили строку')
+    except (ValueError, ZeroDivisionError, TypeError) as exc:
+        return exc
 
 # Тестовые случаи
 numbers = [16, 25, -9, 0, 4.5, "abc"]
