7760e35e9302dc4042a5cee02e644e3ec2094adb
Fantom <andtorg55@yandex.ru>
2024-03-03 17:41:07 +0500

    Finished work

--- Module26/02_files_path/main.py
+++ Module26/02_files_path/main.py
@@ -1,14 +1,15 @@
 # TODO здесь писать код
+"""Импортируем модуль os"""
 import os
 
-# Получаем абсолютный путь к текущей папке
+"""Получаем абсолютный путь до папки выше текущей"""
 folder = os.path.abspath(os.path.join('..'))
 
-# Генерируем список путей к файлам внутри папки
+"""Генерируем список путей до всех файлов внутри папки и подпапок"""
 gen_files_path = [os.path.join(links, file)
                   for links, dirs, files in os.walk(folder)
                   for file in files]
 
-# Печатаем каждый путь к файлу
+"""Выводим каждый полученный путь"""
 for file in gen_files_path:
     print(file)
