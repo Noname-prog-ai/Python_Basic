3ce25f44e69c4a272ccdfcdb1e16673e8d15ca50
Fantom <andtorg55@yandex.ru>
2024-03-04 18:49:46 +0500

    Finished work

--- Module26/03_str_count/main.py
+++ Module26/03_str_count/main.py
@@ -3,17 +3,30 @@ import os
 from collections.abc import Iterable
 
 
+"""Функция strings_count принимает аргумент directory, который указывает на путь к директории, и возвращает генератор из кортежей."""
+"""Каждый кортеж содержит путь к файлу и количество строк кода в файле."""
 def strings_count(directory: str) -> Iterable[tuple]:
+    """
+    Функция для подсчета количества строк кода в файлах с расширением .py в указанной директории и всех ее поддиректориях.
+    :param directory: путь к директории
+    :return: генератор из кортежей (путь к файлу, количество строк кода)
+    """
+    """Обход всех поддиректорий и файлов в указанной директории"""
     for root, dirs, files in os.walk(directory):
         for file in files:
             count = 0
+            """Проверяем, является ли файл файлом с расширением .py"""
             if os.path.join(root, file).endswith('.py'):
                 curr_file = open(os.path.join(root, file), 'r', encoding='utf-8')
+                """Подсчет количества строк кода в файле"""
                 for line in curr_file.readlines():
+                    """Условие для исключения пустых строк и строк, начинающихся с комментариев"""
                     if not (line == '\n' or line.strip().startswith(('"', '#', "'"))):
                         count += 1
+                """Возвращаем путь к файлу и количество строк кода в файле с помощью yield"""
                 yield os.path.join(root, file), count
 
 
+"""Пример использования функции strings_count"""
 for element in strings_count(directory='..'):
     print('Файл "{}": строк кода - {}'.format(element[0], element[1]))
--- Module26/04_linked_list/main.py
+++ Module26/04_linked_list/main.py
@@ -1,14 +1,17 @@
 # TODO здесь писать код
+"""Класс Node представляет узел списка и имеет два атрибута: data (содержит данные узла) и next (ссылка на следующий узел)."""
 class Node:
     def __init__(self, data):
         self.data = data
         self.next = None
 
 
+"""Класс LinkedList представляет сам связный список и имеет атрибут head (ссылка на первый узел в списке)."""
 class LinkedList:
     def __init__(self):
         self.head = None
 
+    """Метод append добавляет новый узел в конец списка."""
     def append(self, data):
         new_node = Node(data)
         if self.head is None:
@@ -19,6 +22,7 @@ class LinkedList:
                 current = current.next
             current.next = new_node
 
+    """Метод get возвращает данные узла по указанному индексу."""
     def get(self, index):
         current = self.head
         count = 0
@@ -29,6 +33,7 @@ class LinkedList:
             current = current.next
         return None
 
+    """Метод remove удаляет узел по указанному индексу."""
     def remove(self, index):
         if index == 0:
             if self.head:
@@ -44,15 +49,18 @@ class LinkedList:
             if current:
                 previous.next = current.next
 
+    """Метод __iter__ позволяет проходить по элементам связного списка при помощи цикла for ... in ...."""
     def __iter__(self):
         current = self.head
         while current:
             yield current.data
             current = current.next
 
+    """Метод __str__ возвращает строковое представление списка."""
     def __str__(self):
         return '[' + ' '.join(str(data) for data in self) + ']'
 
+"""Последние строки кода иллюстрируют использование созданного списка и вызов нескольких его методов для демонстрации работы реализации."""
 my_list = LinkedList()
 my_list.append(10)
 my_list.append(20)
