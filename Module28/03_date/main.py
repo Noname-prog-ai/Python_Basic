d96cd33eafbe033e9a357d4dab92d165f1211fd0
Fantom <andtorg55@yandex.ru>
2024-03-11 19:18:36 +0500

    Finished work

--- Module28/03_date/main.py
+++ Module28/03_date/main.py
@@ -1,1 +1,31 @@
 # TODO здесь писать код
+"""В данном примере мы создаем класс Date с приватными атрибутами _day, _month, _year, которые устанавливаются при создании объекта через конструктор __init__. Мы используем декораторы @classmethod для создания классового метода from_string, который принимает строку в формате dd-mm-yyyy и возвращает объект класса Date, состоящий из соответствующих числовых значений дня, месяца и года."""
+class Date:
+    def __init__(self, day: int, month: int, year: int):
+        self._day = day
+        self._month = month
+        self._year = year
+
+    @classmethod
+    def from_string(cls, date_str: str) -> "Date":
+        day, month, year = map(int, date_str.split('-'))
+        return cls(day, month, year)
+
+    """Метод is_date_valid принимает строку в формате dd-mm-yyyy и проверяет корректность чисел даты. Если дата валидна, то возвращается True, иначе - False."""
+    def is_date_valid(self, date_str: str) -> bool:
+        day, month, year = map(int, date_str.split('-'))
+        # Проверка валидности даты
+        if 1 <= day <= 31 and 1 <= month <= 12 and 1 <= year <= 9999:
+            return True
+        else:
+            return False
+
+    """Метод __str__ переопределяет стандартный метод __str__ и возвращает строку, содержащую значения дня, месяца и года объекта класса Date."""
+    def __str__(self) -> str:
+        return f"день: {self._day}\tмесяц: {self._month}\tгод: {self._year}"
+
+
+date = Date.from_string('10-12-2077')
+print(date)
+print(date.is_date_valid('10-12-2077'))
+print(date.is_date_valid('40-12-2077'))
--- Module28/04_request_caching/main.py
+++ Module28/04_request_caching/main.py
@@ -1,5 +1,51 @@
 # TODO здесь писать код
+class LRUCache:
+    """В конструкторе класса определены три атрибута: "capacity" (емкость кэша), "cache_dict" (словарь для хранения элементов кэша) и "used_keys" (список, отслеживающий порядок использования ключей в кэше)."""
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.cache_dict = {}
+        self.used_keys = []
 
+    """Метод "current_cache" возвращает текущее состояние кэша в виде словаря."""
+    @property
+    def current_cache(self):
+        return self.cache_dict
+
+    """Декоратор "@current_cache.setter" определяет метод "cache", который принимает новый элемент кэша в виде пары ключ-значение и обновляет состояние кэша. Если ключ уже существует в кэше, то он перемещается в конец списка "used_keys" (отслеживающего порядок использования ключей), иначе, если емкость кэша достигнута или превышена, удаляется самый старый элемент (самый первый ключ из "used_keys") и добавляется новый элемент в кэш. Затем список "used_keys" и словарь "cache_dict" обновляются соответственно."""
+    @current_cache.setter
+    def cache(self, new_elem):
+        key, value = new_elem
+        if key in self.cache_dict:
+            self.used_keys.remove(key)
+        elif len(self.cache_dict) >= self.capacity:
+            oldest_key = self.used_keys.pop(0)
+            del self.cache_dict[oldest_key]
+        self.cache_dict[key] = value
+        self.used_keys.append(key)
+
+    """Метод "print_cache" печатает текущее состояние кэша, выводя все пары ключ-значение, отсортированные в порядке использования ключей."""
+    def print_cache(self):
+        print("lRU Cache:")
+        for key in self.used_keys:
+            print(f"{key} : {self.cache_dict[key]}")
+
+    """Метод "get" проверяет, есть ли ключ в кэше. Если ключ будет найден, он будет перемещен в конец списка "used_keys" для отслеживания использования. Затем метод возвращает соответствующее значение. Если ключ не найден, возвращается значение None."""
+    def get(self, key):
+        if key in self.cache_dict:
+            self.used_keys.remove(key)
+            self.used_keys.append(key)
+            return self.cache_dict[key]
+        return None
+
+    """Метод "add" добавляет новый элемент кэша. Если ключ уже существует в кэше, то он перемещается в конец списка "used_keys". Иначе, если емкость кэша достигнута или превышена, удаляется самый старый элемент (самый первый ключ из "used_keys") и добавляется новый элемент в кэш. Затем список "used_keys" и словарь "cache_dict" обновляются соответственно."""
+    def add(self, key, value):
+        if key in self.cache_dict:
+            self.used_keys.remove(key)
+        elif len(self.cache_dict) >= self.capacity:
+            oldest_key = self.used_keys.pop(0)
+            del self.cache_dict[oldest_key]
+        self.cache_dict[key] = value
+        self.used_keys.append(key)
 
 
 # Создаем экземпляр класса LRU Cache с capacity = 3
