3b2855d59ccf5497e48ffa2aeb95e7b76206f842
Fantom <andtorg55@yandex.ru>
2024-03-25 20:37:04 +0500

    homework

--- Module31/04_phone_numbers/main.py
+++ Module31/04_phone_numbers/main.py
@@ -1,1 +1,30 @@
 # TODO здесь писать код
+import re
+
+
+def check_phone_numbers(phone_numbers):
+    for number in phone_numbers:
+        """Проверяем длину номера"""
+        if len(number) != 10:
+            print(f'{number}: не подходит')
+            continue
+
+        """Проверяем первую цифру номера"""
+        if number[0] != '8' and number[0] != '9':
+            print(f'{number}: не подходит')
+            continue
+
+        """Проверяем, что все остальные символы - цифры"""
+        if not re.match(r'^\d+$', number[1:]):
+            print(f'{number}: не подходит')
+            continue
+
+        print(f'{number}: всё в порядке')
+
+
+"""Пример списка номеров"""
+phone_numbers = ['9999999999', '999999-999', '99999x9999']
+
+"""Проверяем номера"""
+check_phone_numbers(phone_numbers)
+
